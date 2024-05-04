76460e0f40b9f1a8974fb6b06de083fb6f26d8c1
kotohasaki <saki.study.mac@gmail.com>
2024-05-04 15:05:05 +0900

    Changes

--- app/src/main/java/xyz/cotoha/program/qa/QandAActivity.java
+++ app/src/main/java/xyz/cotoha/program/qa/QandAActivity.java
@@ -18,9 +18,9 @@ public class QandAActivity extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
 
         List<QandA> qandaList = new ArrayList<>();
-        qandaList.add(new QandA("Q1：メールが届きません", "A：上の文字をタップして詳細を見てください"));
-        qandaList.add(new QandA("Q2：システムアップデートとはなんですか？", "A：上の文字をタップして詳細を見てください"));
-        qandaList.add(new QandA("Q3：アプリのアップデートとはなんですか？", "A：上の文字をタップして詳細を見てください"));
+        qandaList.add(new QandA("Q1：メールが届きません", "A：ここをタップして詳細を見てください"));
+        qandaList.add(new QandA("Q2：システムアップデートとはなんですか？", "A：ここをタップして詳細を見てください"));
+        qandaList.add(new QandA("Q3：アプリのアップデートとはなんですか？", "A：ここをタップして詳細を見てください"));
         qandaList.add(new QandA("Q4：パスワードを忘れてしまった", "A：パスワードの再設定を行ってください \n　（自動入力機能を使うことをおすすめします）"));
         qandaList.add(new QandA("Q5：アプリ内で今まで機能が無くなった", "A：アプリの更新で変更された可能性があります"));
         qandaList.add(new QandA("Q6：間違えて写真などを消してしまった", "A：大抵30日間まではゴミ箱の中に入っています\n　  ゴミ箱から復元してください"));
